522f41aa3af3a8cc57493a8b14ac64770db05f18
Dan Mendelsohn <dan@pomelocare.com>
2022-09-25 15:38:17 -0400

    Solve day 3

--- src/main/kotlin/days/day03/day03.kt
+++ src/main/kotlin/days/day03/day03.kt
@@ -4,16 +4,39 @@ import java.io.File
 
 private const val INPUT_PATH = "inputs/day03.txt"
 
-private fun getInput(): List<String> {
-    return File(INPUT_PATH).readLines()
+typealias Grid = Array<BooleanArray>
+typealias Vector = Pair<Int, Int>  // down, right
+
+private fun getInput(): Grid {
+    return File(INPUT_PATH).readLines().map { line -> line.map { it == '#' }.toBooleanArray() }.toTypedArray()
 }
 
-private fun part1(): Any {
-    return "Not implemented"
+private fun countTreesOnVector(grid: Grid, vector: Vector): Int {
+    var position = Pair(0, 0)  // row num, col num
+    var numTrees = 0
+    while (position.first < grid.size) {
+        if (grid[position.first][position.second % grid[0].size]) {
+            numTrees++
+        }
+        position = Pair(position.first + vector.first, position.second + vector.second)
+    }
+    return numTrees
 }
 
-private fun part2(): Any {
-    return "Not implemented"
+private fun part1(): Int {
+    val grid = getInput()
+    return countTreesOnVector(grid, Pair(1, 3))
+}
+
+private fun part2(): Long {
+    val grid = getInput()
+    return listOf(
+        Pair(1, 1),
+        Pair(1, 3),
+        Pair(1, 5),
+        Pair(1, 7),
+        Pair(2, 1),
+    ).foldRight(1L) { vector, acc -> acc * countTreesOnVector(grid, vector) }
 }
 
 
