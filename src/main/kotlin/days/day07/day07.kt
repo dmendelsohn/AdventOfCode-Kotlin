fb088aa66e0538daab4da6ad27b753fdc2d004a3
Dan Mendelsohn <dan@pomelocare.com>
2022-09-27 11:42:19 -0400

    Day 6

---
+++ src/main/kotlin/days/day07/day07.kt
@@ -0,0 +1,23 @@
+package days.day07
+
+import java.io.File
+
+private const val INPUT_PATH = "inputs/day07.txt"
+
+private fun getInput(): List<String> {
+    return File(INPUT_PATH).readLines()
+}
+
+private fun part1(): Any {
+    return "Not implemented"
+}
+
+private fun part2(): Any {
+    return "Not implemented"
+}
+
+
+fun main(args: Array<String>) {
+    println("Solution to part 1: ${part1()}")
+    println("Solution to part 2: ${part2()}")
+}
