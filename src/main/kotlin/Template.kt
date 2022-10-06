a14c3aec5ba87c281418cb9f49abb9807ed2489e
Dan Mendelsohn <dan@pomelocare.com>
2022-10-05 22:43:44 -0400

    Update template

---
+++ src/main/kotlin/Template.kt
@@ -0,0 +1,28 @@
+import java.io.File
+
+private val DEFAULT_INPUT_PATH = "inputs/${object {}.javaClass.packageName.split(".").last()}.txt"
+
+private fun getRawInput(inputPath: String): String {
+    return File(inputPath).readText().trim()
+}
+
+fun parseInput(rawInput: String): List<String> {
+    return rawInput.split("\n")
+}
+
+fun part1(input: List<String>): Any {
+    return "Not implemented"
+}
+
+fun part2(input: List<String>): Any {
+    return "Not implemented"
+}
+
+
+fun main(args: Array<String>) {
+    val inputPath = args.firstOrNull() ?: DEFAULT_INPUT_PATH
+    println("Running using input from $inputPath")
+    val input = parseInput(getRawInput(inputPath))
+    println("Solution to part 1: ${part1(input)}")
+    println("Solution to part 2: ${part2(input)}")
+}
